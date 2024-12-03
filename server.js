0c592609ae447d0663f0b116e459f8c586c8deca
Lucas Baez <49772747+LuDeLu@users.noreply.github.com>
2024-12-03 14:06:34 -0300

    fasd

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Manejo de errores no capturados
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  // Realizar tareas de limpieza si es necesario
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Realizar tareas de limpieza si es necesario
+});
+
+// Manejo de cierre del servidor
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received.');
+  console.log('Closing HTTP server.');
+  server.close(() => {
+    console.log('HTTP server closed.');
+    // Cerrar conexiones de base de datos u otros recursos
+    process.exit(0);
+  });
+});
+
